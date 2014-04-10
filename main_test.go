cd24f3afec967f8c19f0717a8c18ec499ba32398
Gianfranco Reppucci <gianfranco.reppucci@gmail.com>
2014-04-10 16:55:42 +0200

    Tests

---
+++ main_test.go
@@ -0,0 +1,12 @@
+package timetasks
+
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFallisciSicuramente(t *testing.T) {
+	Convey("Testiamo qualcosa", t, func() {
+		So(1, ShouldEqual, 2)
+	})
+}
